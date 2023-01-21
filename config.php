1364e8f36964918f26fd9b168c786477e055dcb0
Karina Chirva <ckchirva@gmail.com>
2023-01-21 22:38:52 +0100

    Projekt-Php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// konfiguracja bazy danych oraz innych parametrów
+	$db_type="pgsql";
+	$db_host="gargamel.wsb.poznan.pl";
+	$db_port="5432";
+	$db_name="pzx102908";
+	$db_user="pzx102908";
+	$db_password="eingae6hahNgu6lahs5a";
+	$losowana_liczba_fiszek=12;
+	$poprawna_odpowiedz=2;
+	$niepoprawna_odpowiedz=-1;
+?>
